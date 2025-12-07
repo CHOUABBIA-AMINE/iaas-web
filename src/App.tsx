b94da48bef719cbc67490f4e1bbce1b27752754d
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-07 13:49:25 +0100

    refactor: reformat App.tsx - decompress minified code for readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import{BrowserRouter as Router,Routes,Route}from"react-router-dom"
-import{ThemeProvider}from"@mui/material/styles"
-import CssBaseline from"@mui/material/CssBaseline"
-import theme from"./theme/theme"
-import Layout from"./components/Layout/Layout"
-import HomePage from"./pages/HomePage"
-import LoginPage from"./pages/LoginPage"
-function App(){return(<ThemeProvider theme={theme}><CssBaseline/><Router><Layout><Routes><Route path="/"element={<HomePage/>}/><Route path="/login"element={<LoginPage/>}/></Routes></Layout></Router></ThemeProvider>)}
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme/theme'
+import Layout from './components/Layout/Layout'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  )
+}
+
 export default App
