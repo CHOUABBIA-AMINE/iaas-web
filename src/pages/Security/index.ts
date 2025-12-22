1f918eed7af181b66d94d4c913b3b4c1791394d5
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-22 12:28:44 +0100

    Clean old frontend folders in favor of module-based architecture

--- src/pages/Security/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as UserList } from './UserList'
-export { default as UserEdit } from './UserEdit'
-export { default as RoleList } from './RoleList'
-export { default as RoleEdit } from './RoleEdit'
-export { default as GroupList } from './GroupList'
-export { default as GroupEdit } from './GroupEdit'
-export { default as ResetPassword } from './ResetPassword'
