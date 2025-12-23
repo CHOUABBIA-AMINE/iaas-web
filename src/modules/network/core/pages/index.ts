cb4049d3bbbf2cd845f087d22b158b690ba4a3b3
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 13:53:12 +0100

    Add HydrocarbonField pages to index

--- src/modules/network/core/pages/index.ts
+++ src/modules/network/core/pages/index.ts
@@ -2,3 +2,5 @@ export { default as StationList } from './StationList';
 export { default as StationEdit } from './StationEdit';
 export { default as TerminalList } from './TerminalList';
 export { default as TerminalEdit } from './TerminalEdit';
+export { default as HydrocarbonFieldList } from './HydrocarbonFieldList';
+export { default as HydrocarbonFieldEdit } from './HydrocarbonFieldEdit';
