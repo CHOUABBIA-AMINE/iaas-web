b49a061c5d2ba5ea42a10c8ffc37a04dbe57876b
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 14:42:11 +0100

    feat: add CountryDTO aligned with backend

---
+++ src/modules/common/administration/dto/CountryDTO.ts
@@ -0,0 +1,15 @@
+/**
+ * Country DTO
+ * Data Transfer Object for Country entity
+ * 
+ * @author CHOUABBIA Amine
+ * @created 12-23-2025
+ */
+
+export interface CountryDTO {
+  id: number;
+  name: string;
+  code?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
