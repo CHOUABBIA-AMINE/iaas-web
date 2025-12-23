4374e98b8333b82ce1072e2f1f84cfde87f6f0a4
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 14:43:36 +0100

    feat: create business.consultation module dto structure

---
+++ src/modules/business/consultation/dto/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Consultation Module DTOs
+ * Data Transfer Objects for Consultation business operations
+ * 
+ * @author CHOUABBIA Amine
+ * @created 12-23-2025
+ */
+
+// Export consultation DTOs here
