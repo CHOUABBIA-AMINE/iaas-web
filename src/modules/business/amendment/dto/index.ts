2d2f8171b3ca9766245633c187643d127f7f25cd
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 14:43:33 +0100

    feat: create business.amendment module dto structure

---
+++ src/modules/business/amendment/dto/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Amendment Module DTOs
+ * Data Transfer Objects for Amendment business operations
+ * 
+ * @author CHOUABBIA Amine
+ * @created 12-23-2025
+ */
+
+// Export amendment DTOs here
