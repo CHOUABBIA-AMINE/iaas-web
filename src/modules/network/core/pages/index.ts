dc944ff93e5d1f905cd99ede6909e0baaed43d2f
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 13:35:42 +0100

    Add Terminal pages index

--- src/modules/network/core/pages/index.ts
+++ src/modules/network/core/pages/index.ts
@@ -1,2 +1,4 @@
 export { default as StationList } from './StationList';
 export { default as StationEdit } from './StationEdit';
+export { default as TerminalList } from './TerminalList';
+export { default as TerminalEdit } from './TerminalEdit';
