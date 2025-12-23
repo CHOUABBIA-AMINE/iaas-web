153407180cb4e3880f9d78df6c5e9b86738c8f5e
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 23:01:13 +0100

    Add barrel export file for all DTOs

---
+++ src/shared/dto/index.ts
@@ -0,0 +1,46 @@
+// Consultation DTOs
+export * from './consultation/AwardMethodDto';
+export * from './consultation/ConsultationDto';
+export * from './consultation/ConsultationPhaseDto';
+export * from './consultation/ConsultationStepDto';
+export * from './consultation/SubmissionDto';
+
+// Contract DTOs
+export * from './contract/ContractDto';
+export * from './contract/ContractItemDto';
+export * from './contract/ContractPhaseDto';
+export * from './contract/ContractStepDto';
+export * from './contract/ContractTypeDto';
+
+// Amendment DTOs
+export * from './amendment/AmendmentDto';
+export * from './amendment/AmendmentPhaseDto';
+export * from './amendment/AmendmentStepDto';
+export * from './amendment/AmendmentTypeDto';
+
+// Plan DTOs
+export * from './plan/BudgetModificationDto';
+export * from './plan/BudgetTypeDto';
+export * from './plan/DomainDto';
+export * from './plan/FinancialOperationDto';
+export * from './plan/ItemDto';
+export * from './plan/ItemDistributionDto';
+export * from './plan/ItemStatusDto';
+export * from './plan/PlannedItemDto';
+export * from './plan/RubricDto';
+
+// Provider DTOs
+export * from './provider/ClearanceDto';
+export * from './provider/EconomicDomainDto';
+export * from './provider/EconomicNatureDto';
+export * from './provider/ExclusionTypeDto';
+export * from './provider/ProviderDto';
+export * from './provider/ProviderExclusionDto';
+export * from './provider/ProviderRepresentatorDto';
+
+// Core DTOs
+export * from './core/ApprovalStatusDto';
+export * from './core/CurrencyDto';
+export * from './core/ProcurementDirectorDto';
+export * from './core/ProcurementNatureDto';
+export * from './core/ProcurementStatusDto';
