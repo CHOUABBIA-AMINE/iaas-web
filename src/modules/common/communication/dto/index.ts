73a21b95411f21011bb243595be039d21a3e803a
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 14:43:56 +0100

    feat: create common.communication module dto structure

---
+++ src/modules/common/communication/dto/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Communication Module DTOs
+ * Data Transfer Objects for Communication operations
+ * 
+ * @author CHOUABBIA Amine
+ * @created 12-23-2025
+ */
+
+// Export communication DTOs here
