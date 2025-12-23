fb399a1faa8a8826ce08995003b126bc06eaa8e5
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-23 14:42:49 +0100

    feat: add administration module index for organized exports

---
+++ src/modules/common/administration/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Administration Module
+ * Exports for Administration sub-module
+ * 
+ * @author CHOUABBIA Amine
+ * @created 12-23-2025
+ */
+
+export * from './dto';
+export * from './services';
