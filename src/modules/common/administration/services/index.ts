b21a5776d7b2281eb69bb6d7e8d72c23595fd785
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-28 14:33:14 +0100

    fix: correct service exports to match actual export patterns

--- src/modules/common/administration/services/index.ts
+++ src/modules/common/administration/services/index.ts
@@ -1,4 +1,7 @@
-export { default as localityService } from './localityService';
-export { default as stateService } from './stateService';
-export { default as StructureService } from './StructureService';
-export { default as StructureTypeService } from './StructureTypeService';
+// Named exports from existing services
+export { localityService } from './localityService';
+export { stateService } from './stateService';
+
+// Default exports from new services
+export { default as structureService } from './StructureService';
+export { default as structureTypeService } from './StructureTypeService';
