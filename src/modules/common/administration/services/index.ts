e9ec5c26c3dac27cc7354950db8f3338cacd76eb
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2025-12-28 18:55:22 +0100

    feat: update exports for Job DTO, service and components

--- src/modules/common/administration/services/index.ts
+++ src/modules/common/administration/services/index.ts
@@ -5,3 +5,4 @@ export { stateService } from './stateService';
 // Default exports from new services
 export { default as structureService } from './StructureService';
 export { default as structureTypeService } from './StructureTypeService';
+export { default as jobService } from './JobService';
