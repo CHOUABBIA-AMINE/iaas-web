97bc45cd6d35119fc75dc0caf5b0d97418101c4b
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-01 12:17:06 +0100

    feat(network): add paging+search to pipeline service

--- src/modules/network/core/services/pipelineService.ts
+++ src/modules/network/core/services/pipelineService.ts
@@ -9,12 +9,38 @@
 
 import axios from '../../../../shared/config/axios';
 import { PipelineDTO, PipelineCreateDTO, PipelineUpdateDTO } from '../dto';
+import { PageResponse } from '../../../../shared/types/PageResponse';
 
 const API_BASE = '/network/core/pipeline';
 
 class PipelineService {
   async getAll(): Promise<PipelineDTO[]> {
     const response = await axios.get(`${API_BASE}/all`);
+    return response.data;
+  }
+
+  async getPage(
+    page: number = 0,
+    size: number = 20,
+    sortBy: string = 'id',
+    sortDir: string = 'asc'
+  ): Promise<PageResponse<PipelineDTO>> {
+    const response = await axios.get<PageResponse<PipelineDTO>>(API_BASE, {
+      params: { page, size, sortBy, sortDir }
+    });
+    return response.data;
+  }
+
+  async search(
+    query: string,
+    page: number = 0,
+    size: number = 20,
+    sortBy: string = 'id',
+    sortDir: string = 'asc'
+  ): Promise<PageResponse<PipelineDTO>> {
+    const response = await axios.get<PageResponse<PipelineDTO>>(`${API_BASE}/search`, {
+      params: { q: query, page, size, sortBy, sortDir }
+    });
     return response.data;
   }
 
