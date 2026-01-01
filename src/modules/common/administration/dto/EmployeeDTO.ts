cf9ac5e93fd3bde367d1ac4224962bcf0b3dbb4d
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-01 08:35:19 +0100

    chore: add f_01 to EmployeeDTO (Person field)

--- src/modules/common/administration/dto/EmployeeDTO.ts
+++ src/modules/common/administration/dto/EmployeeDTO.ts
@@ -2,18 +2,27 @@
  * Employee DTO
  * @author CHOUABBIA Amine
  * @created 12-30-2025
+ * @updated 01-01-2026 - Add f_01 (Person field)
  */
 
 export interface EmployeeDTO {
   id?: number;
+
+  // Person fields
+  f_01?: string;
+
+  // Names
   lastNameAr: string;
   firstNameAr: string;
   lastNameLt: string;
   firstNameLt: string;
+
   birthDate?: string;
   birthPlace?: string;
+
   countryId?: number;
   registrationNumber?: string;
+
   jobId?: number;
   structureId?: number;
   militaryRankId?: number;
