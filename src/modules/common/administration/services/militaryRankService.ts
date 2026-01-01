19950154f852fe2ac8db4161966605d26c2705e6
Aminosoft <147345045+CHOUABBIA-AMINE@users.noreply.github.com>
2026-01-01 07:51:08 +0100

    fix: use militaryRank endpoint in militaryRankService
    - Backend uses /common/administration/militaryRank (camelCase)
    - Fixes 404 when loading employee edit lookup data

--- src/modules/common/administration/services/militaryRankService.ts
+++ src/modules/common/administration/services/militaryRankService.ts
@@ -2,12 +2,14 @@
  * Military Rank Service
  * @author CHOUABBIA Amine
  * @created 12-30-2025
+ * @updated 01-01-2026 - Fix endpoint (militaryRank)
  */
 
 import axiosInstance from '../../../../shared/config/axios';
 import { MilitaryRankDTO } from '../dto';
 
-const BASE_URL = '/common/administration/military-rank';
+// Backend endpoint uses camelCase: militaryRank
+const BASE_URL = '/common/administration/militaryRank';
 
 export const militaryRankService = {
   getAllList: async (): Promise<MilitaryRankDTO[]> => {
